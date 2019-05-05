876a3264056d849d455a43a1428dd35ba1f9a294
lulu751 <1908498945@qq.com>
2019-05-05 16:05:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/hp/myexperiment4/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.hp.myexperiment4;
+
+import android.os.Bundle;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Display the fragment as the main content.
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
+}
